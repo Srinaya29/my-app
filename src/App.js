cfbdabeb5de0a4cdb115be44288f925d46110de6
Srinaya29 <sydugarisrinaya@gmail.com>
2025-03-05 21:10:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
 }
-
 export default App;
