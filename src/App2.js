01b8322ff4742fb8753b9bf70d93b8b87cf134a2
Srinaya29 <sydugarisrinaya@gmail.com>
2025-03-07 14:06:49 +0530

    added

--- src/App2.js
+++ src/App2.js
@@ -3,16 +3,18 @@ import {useState} from "react";
 export default function App2() {
     const [count, setCount]=useState(0);
     const increment=() =>{
-    setCount(count+1)   
-    }
+    setCount(count+1);   
+    };
     const decrement=() =>{
-        count=count-1;    
-        }
+    setCount(count-1);    
+    };
   return (
     <div>
         <button onClick={decrement}>-</button>
         {count}
-        <button onclick={increment}>+</button>
+
+
+        <button onClick={increment}>+</button>
     </div>
   );
 }
